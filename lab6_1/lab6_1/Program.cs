afc05304db1e6cfeb51968774711f46d03f1325f
assilbek <abdullaev01a@gmail.com>
2017-12-20 15:34:48 +0600

    As

---
+++ lab6_1/lab6_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            book b1 = new book();
+            b1.SetBook("Достоевский Ф.М.", "Братья Карамазовы", "Вильямс", 2035, 2000);
+            book.SetPrice(12);
+            b1.Show();
+            Console.WriteLine("\n Finally price for rent: {0} p.", b1.PriceBook(3));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab6_1/lab6_1/book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6_1
+{
+    class book
+    {
+        private String author;
+        private String title;
+        private String publisher;
+        private int pages;
+        private int year;
+        private static double price = 9;
+        public void SetBook(String author, String title, String publisher, int pages, int year)
+        {
+            this.author = author;
+            this.title = title;
+            this.publisher = publisher;
+            this.pages = pages;
+            this.year = year;
+
+        }
+        public static void SetPrice(double price)
+        {
+            book.price = price;
+        }
+        public void Show()
+        {
+            Console.WriteLine("\nBook: \n Author: {0}\n Name: {1}\n Year: {2}\n {3}pages\n Rent price: {4}", author, title, year, pages, book.price);
+
+        }
+        public double PriceBook(int s)
+        {
+            double cust = s * price;
+            return cust;
+        }
+
+    }
+}
